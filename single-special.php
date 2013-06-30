2dd597c4841cbc6b5646f70f20d6eb3a7a203760
mhuijser <mhuijser@gmail.com>
2013-06-30 17:08:10 +0200

    Adding single-templates for gemeenten to the Rafael Theme

---
+++ single-special.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Sample template for displaying single gemeente posts.
+ * Save this file as as single-gemeente.php in your current theme.
+ *
+ * This sample code was based off of the Starkers Baseline theme: http://starkerstheme.com/
+ */
+
+get_header(); ?>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+<h1><?php the_title(); ?></h1>&nbsp;
+<?php the_content(); ?> &nbsp;
+
+<?php endwhile; // end of the loop. ?>
+
+<?php get_footer();  ?>
